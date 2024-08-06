8e57fc01208f1b2f7bf269b87360f32a2117d75b
Moyboy <Vincent.moy94@gmail.com>
2024-08-05 22:51:08 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(function (driver){
+        return driver.toLowerCase() === name.toLowerCase()
+    })
+}
+
+function fuzzyMatch(drivers, search) {
+    let searchLength = search.length;
+
+    return drivers.filter(function (driver){
+        return driver.substring(0,searchLength) === search
+    })
+}
+
+function matchName(drivers, something){
+    return drivers.filter(function (driver){
+        return driver.name === something
+    })
+}
